35869e2abe34bb678d00ad898b0042926534b803
Linda <Linda@LAPTOP-R1KOF62A>
2022-10-21 14:38:34 +0200

    Add project files.

---
+++ Triangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol;
+            int width;
+
+            Console.Write("Please enter a symbol to paint: \n");
+            symbol = Console.ReadLine();
+
+            Console.Write("Please enter the width of the triangle:\n");
+            width = int.Parse(Console.ReadLine());
+
+            string[][] Triangle = new string[width][];
+            
+   
+            for(int i=0; i<width;i++)
+            {
+                Triangle[i] = new string[width-i];
+            }
+
+            int k = width;
+            for(int j = 0; j < width;j++)
+            {
+                for(int l= k; l > 0; l-- )
+                {
+                    Triangle[j][l - 1] = symbol;
+                    Console.Write(Triangle[j][l - 1].ToString());
+                    
+                }
+                Console.WriteLine();
+                k--;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
